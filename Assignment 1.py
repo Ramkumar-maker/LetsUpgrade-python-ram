ae3ef865dd479f4f2643337e6eb5364fb90b44c4
Ramkumar-maker <68571027+Ramkumar-maker@users.noreply.github.com>
2020-07-21 05:28:26 +0530

    Create Assignment 1.py

---
+++ Assignment 1.py
@@ -0,0 +1,11 @@
+What is Python?
+Python is an interpreted, object-oriented, high-level programming language with dynamic semantics. ... 
+Python's simple, easy to learn syntax emphasizes readability and therefore reduces the cost of program maintenance. 
+Python supports modules and packages, which encourages program modularity and code reuse.
+
+What are the ways to install python in your PC?
+Anaconda
+
+What are the code Environmts? Types?
+A code environment is a standalone and self-contained environment to run Python or R code. Each code environment has its own set of packages.
+Environments are independent: you can install different packages or different versions of packages in different environments without interaction between them.
